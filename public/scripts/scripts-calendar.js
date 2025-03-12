d9207d75357ead7e786fc575c4e2c6722eb4c7aa
Rodrigo Sanchez <rodrigo.aaron.sa@gmail.com>
2025-03-12 09:24:59 -0700

    Merged Rodrigo code with the one in the repo

--- public/scripts/scripts-calendar.js
+++ public/scripts/scripts-calendar.js
@@ -1,5 +1,16 @@
 $(document).ready(function() {
     $('#calendar').evoCalendar({
     
+        calendarEvents: [{
+            id: 'event1',
+            name: 'Event Name',
+            date: 'March/12/2025',
+            description:"This is a test description",
+            type: "holiday",
+            everyYear: true
+        
+        }
+        ]
     })
 })
+
