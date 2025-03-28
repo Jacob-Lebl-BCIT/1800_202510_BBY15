debcde2b06ccf0217cc2b16b20b32e061f79445b
Calvin Chan <cchan572@my.bcit.ca>
2025-03-28 09:05:23 -0400

    Creating notes js

--- public/scripts/button.js
+++ public/scripts/button.js
@@ -114,3 +114,36 @@ async function userClick() {
         // TODO: Update the UI to show the new note, e.g., by adding a new card also sync it with our "all notes" section
     }
 }
+
+function displayUserNotes() {
+    var notesRef = db.collection("notes");
+    const timestamp = firebase.firestore.FieldValue.serverTimestamp();
+    notesRef.add({
+        title: formValues.title,
+        content: formValues.content,
+        timestamp: timestamp,
+        lastModified: timestamp
+    });
+    displayCardsDynamically("notes");
+}
+function displayCardsDynamically(collection) {
+    let cardTemplate = document.getElementById("noteCardTemplate"); // Retrieve the HTML element with the ID "hikeCardTemplate" and store it in the cardTemplate variable. 
+
+    db.collection(collection).get()   //the collection called "notes"
+        .then(allNotes=> {
+            //var i = 1;  //Optional: if you want to have a unique ID for each hike
+            allNotes.forEach(doc => { //iterate thru each doc
+                var title = doc.data().title;       // get value of the "title" key
+                var details = doc.data().content;  // get value of the "content" key
+                let newcard = cardTemplate.cloneNode(true); // Clone the HTML template to create a new card (newcard) that will be filled with Firestore data.
+
+                //update title and text and image
+                newcard.querySelector('.card-title').innerHTML = title;
+                newcard.querySelector('.card-text').innerHTML = details;
+                newcard.querySelector('.card-subtitle').innerHTML = "Card subtitle";
+
+                document.getElementById("my-notes").appendChild(newcard);
+            })
+        })
+        // cardTemplate.classList.add("d-none");
+}
