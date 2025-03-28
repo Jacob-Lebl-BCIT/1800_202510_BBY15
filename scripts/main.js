7c4546c4a41afdb48af8a0307420c59c1586de2f
Jacob Lebl <lebl.jacob@gmail.com>
2025-03-28 00:26:02 -0700

    working on notes display in index

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,4 @@
+// will contain user id when logged in
 var currentUser;
 
 document.addEventListener("DOMContentLoaded", (event) => {
@@ -5,6 +6,7 @@ document.addEventListener("DOMContentLoaded", (event) => {
     //Function that calls everything needed for the main page  
     function doAll() {
         firebase.auth().onAuthStateChanged(user => {
+            console.log("Auth state changed");
             if (user) {
                 currentUser = db.collection("users").doc(user.uid); //global
                 console.log(currentUser);
@@ -15,6 +17,8 @@ document.addEventListener("DOMContentLoaded", (event) => {
                 insertNavBar();
 
                 insertNewNoteButton();
+
+                displayNotes();
 
             } else {
                 // No user is signed in.
@@ -76,8 +80,40 @@ function insertNewNoteButton() {
 
 }
 
+// Function to fetch user notes from Firestore
+async function fetchUserNotes() {
+    const user = firebase.auth().currentUser;
+    if (user) {
+        const notesSnapshot = await db.collection("users").doc(user.uid).collection("notes").orderBy("timestamp", "desc").get();
+        return notesSnapshot.docs.map(doc => doc.data());
+    }
+    return [];
+}
+
+// display notes function
+function displayNotes() {
+    currentUser.get().then(userDoc => {
+        //currentUser.get() returns a promise with snapshot of the user document (firebase. firestore. QuerySnapshot < T > )
+
+        // get latest notes from user
+        // notes contain:
+        // lastModified
+        // timestamp
+        // title
+        // content
+
+        return db.collection("notes").where("userId", "==", currentUser.id).orderBy("lastModified", "desc").get();
 
 
+    }).then(notes => {
+
+        console.log("Notes: ", notes.docs);
+
+    });
+}
+
+
+loadNotes = new Promise((resolve, reject) => {});
 
 
 
